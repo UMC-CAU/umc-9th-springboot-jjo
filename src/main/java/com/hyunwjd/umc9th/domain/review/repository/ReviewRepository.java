660606bea124d7931ee2638fe02e80642bc9d5cb
hyun-wjd <helencho0530@gmail.com>
2025-10-29 17:37:51 +0900

    refactor: 연관 경로 명시

--- src/main/java/com/hyunwjd/umc9th/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/hyunwjd/umc9th/domain/review/repository/ReviewRepository.java
@@ -11,18 +11,18 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    // 1) 가게별 리뷰 페이징 (메서드 이름 기반)
-    Page<Review> findByStoreId(Long storeId, Pageable pageable);
+    // 1) 가게별 리뷰 페이징 (메서드 이름 기반) 연관경로는 _로 연결
+    Page<Review> findByStore_Id(Long storeId, Pageable pageable);
 
-    // 2) 회원별 최신 리뷰 10개 (메서드 이름 기반)
-    List<Review> findTop10ByMemberIdOrderByCreatedAtDesc(Long memberId);
+    // 2) 회원별 최신 리뷰 20개 (메서드 이름 기반)
+    List<Review> findTop20ByMemberIdOrderByCreatedAtDesc(Long memberId);
 
     // 3) 특정 회원이 특정 가게에 리뷰를 남겼는지 (메서드 이름 기반)
     boolean existsByMemberIdAndStoreId(Long memberId, Long storeId);
 
-    // 4) 가게 평균 별점 (JPQL @Query)
-    @Query("select avg(r.starGrade) from Review r where r.store.id = :storeId")
-    Double findAvgStarByStoreId(@Param("storeId") Long storeId);
+    // 4) 사용자가 남긴 별점 (메서드 이름 기반)
+    List<Integer> findStarGradeByMember_Id(Long memberId);
+
 
     // 5) 가게의 리뷰 수 (메서드 이름 기반)
     long countByStoreId(Long storeId);
